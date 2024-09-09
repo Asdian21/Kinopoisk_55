5929f9e4dbc176cde91a7444fef3f33df78b2c2a
Asdian <asdiank7@gmail.com>
2024-09-09 17:57:37 +0500

    add function to change theme to body

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let changeThemeBtn = document.querySelector(".themeChange");
+let body = document.querySelector("body");
 
 changeThemeBtn.addEventListener("click", changeTheme);
 
 function changeTheme() {
   changeThemeBtn.classList.toggle("darkTheme");
+  body.classList.toggle("dark");
 }
