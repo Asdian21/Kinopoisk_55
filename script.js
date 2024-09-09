240b5305632963569011ba7b51168dc1e022352f
Asdian <asdiank7@gmail.com>
2024-09-10 01:07:37 +0500

    add localStorage to detect and save theme

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ function changeTheme() {
   changeThemeBtn.classList.toggle("darkTheme");
   body.classList.toggle("dark");
 }
+
+if (localStorage.getItem("theme") == "dark") {
+  changeThemeBtn.classList.add("darkTheme");
+  body.classList.add("dark");
+}
+
+function changeTheme() {
+  if (localStorage.getItem("theme") == "dark") {
+    // сменить на белую
+    changeThemeBtn.classList.remove("darkTheme");
+    body.classList.remove("dark");
+    localStorage.setItem("theme", "white");
+  } else {
+    // сменить на темную
+    changeThemeBtn.classList.add("darkTheme");
+    body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+}
+
+let searchBtn = document.querySelector(".search button");
+searchBtn.addEventListener("click", searchMovie);
+
+let loader = document.querySelector(".loader");
+
+function searchMovie() {
+  let searchInput = document.querySelector(".search input").value;
+}
+
+// let changeColorBtn = document.querySelector(".save");
+
+// changeColorBtn.addEventListener("click", favourite);
+
+// function favourite() {
+//   changeColorBtn.classList.toggle("saved");
+// }
