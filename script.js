b0283fb7cc7b7ef7fb8a40f426694f8180d9acf7
Asdian <asdiank7@gmail.com>
2024-09-11 18:13:53 +0500

    add apikey and sendRequest function

--- script.js
+++ script.js
@@ -32,9 +32,44 @@ searchBtn.addEventListener("click", searchMovie);
 
 let loader = document.querySelector(".loader");
 
-function searchMovie() {
-  let searchInput = document.querySelector(".search input").value;
+async function searchMovie() {
+  loader.style.display = "block";
+
+  let searchText = document.getElementById("dd").value;
+  console.log(searchText);
+
+  let response = await sendRequest("http://www.omdbapi.com/", "GET", {
+    apikey: "54b23d27",
+    t: searchText,
+  });
+  console.log(response);
 }
+
+async function sendRequest(url, method, data) {
+  if (method == "POST") {
+    let response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    response = await response.json();
+    return response;
+  } else if (method == "GET") {
+    url = url + "?" + new URLSearchParams(data);
+    let response = await fetch(url, {
+      method: "GET",
+    });
+
+    response = await response.json();
+    return response;
+  }
+}
+
+// асинхронная функция возвращает промис, когда запрос отправился, но ты не знаешь, когда придёт ответ
 
 // let changeColorBtn = document.querySelector(".save");
 
